c5549e5152f845757e89ac451922acccefdecfb7
JoelJeremy <amahj@roc-dev.com>
2022-03-24 09:19:26 +0100

    opdracht 3.1 tm 3,5

--- H03/3.5.php
+++ H03/3.5.php
@@ -1,1 +1,32 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+<meta charset=" UTF-8">
+    <title> van while naar for-loop </title>
+</head>
+
+<body>
+
 <?php
+
+echo "<h1> Hier begint de while loop </h1>";
+echo "<p> Een while loop gebruik je als je niet van te voren kunt weten hoeveel herhalimgen je wilt
+(dit is afhankelijk van een extern gegeven).</p>"
+;
+$x = 35;
+while  ($x >= 25) {
+    echo "samsung ";
+    $x--;
+}
+
+echo "<h1> Hier begint de for loop </h1>";
+echo "<p> Een voorloop gebruik je als je exact weet hoeveel herhalingen je wilt </p> ";
+
+for( $i = 35; $i >= 25; $i--) {
+    echo"s22ultra ";
+}
+?>
+
+</body>
+</html>
