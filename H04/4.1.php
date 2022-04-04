937e18c2bba95b6f487918d24ce4a08b27ff21bf
JoelJeremy <amahj@roc-dev.com>
2022-04-04 10:34:18 +0200

    opdracht 4.1

---
+++ H04/4.1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> H4.1: Van Celsius naar Fahrenheit </title>
+</head>
+
+<body>
+<?php
+
+for( $i = -10; $i <= 40; $i++) {
+    CalculateFahrenheitFromCelsius($i);
+}
+
+function CalculateFahrenheitFromCelsius($celsius) {
+    $fahrenheit = $celsius * 1.8 + 32;
+    echo "{$celsius} degrees celsius amounts to {$fahrenheit} degrees Fahrenheit.<br>";
+}
+
+?>
+</body>
+</html>
